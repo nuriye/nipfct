ee6358fbfb6d6496a3e0e7ffd9940bf6fdd3fd40
Theresa <theresa.c.schmidt@outlook.com>
2018-01-20 16:54:05 +0100

    navbar height icons welcome, home Spieleuebersicht

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import {Router} from '@angular/router';
+import {AuthService} from '../core/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  email = '';
+  password = '';
+  errorMessage = '';
+  username = '';
+  error: {name: string, message: string} = {name: '', message: ''};
+
+
+  constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit() {
   }
+
+  clearErrorMessage() {
+    this.errorMessage = '';
+    this.error = { name: '', message: '' };
+  }
+
+  onSignUp(): void {
+    this.clearErrorMessage()
+ 
+    if (this.email, this.password, this.username) {
+      this.authService.signUpWithEmail(this.email, this.password, this.username)
+        .then(() => {
+          this.router.navigate(['/home'])
+        })
+        .catch(_error => {
+          this.error = _error
+          this.router.navigate(['/'])
+        })
+    }
+  }
+
 
 }
