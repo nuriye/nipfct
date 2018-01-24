b958c885464363e9bcf2d89798b511025d064904
Theresa <theresa.c.schmidt@outlook.com>
2018-01-24 13:52:05 +0100

    Input aus app-mdoeule rausfgen. users-in-game del

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Input } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AngularFireModule} from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
